ba9d38156ef34abdd94925695b2ad248bcb4aec3
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-02 20:16:29 +0200

    basic examples

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ExtensionMethods.TCPData;
-using ExtensionMethods.TCPExtensions;
+
+// ################################################
+// ###### LINQ Query Synthax vs TSQL Synthax ######
+// ################################################
 
 //List<Employee> employeeList = Data.GetEmployees();
 
@@ -17,15 +20,49 @@ using ExtensionMethods.TCPExtensions;
 //    Console.WriteLine();
 //};
 
-List<Department> deparmentList = Data.GetDepartments();
+//List<Department> deparmentList = Data.GetDepartments();
 //var filteredDeparments = deparmentList.Filter(dept => dept.ShortName == "HR" || dept.ShortName == "IT");
-var filteredDeparments = deparmentList.Filter(dept => dept.Id > 1);
+//var filteredDeparments = deparmentList.Filter(dept => dept.Id > 1);
 
-foreach (var deparment in filteredDeparments)
+//foreach (var deparment in filteredDeparments)
+//{
+
+//    Console.WriteLine($"Id: {deparment.Id}");
+//    Console.WriteLine($"Short Name: {deparment.ShortName}");
+//    Console.WriteLine($"Long Name: {deparment.LongName}");
+//    Console.WriteLine();
+//};
+
+
+List<Employee> employeeList = Data.GetEmployees();
+List<Department> deparmentList = Data.GetDepartments();
+
+var resultList = from emp in employeeList
+                 join dept in deparmentList
+                 on emp.DepartmentId equals dept.Id
+                 select new
+                 {
+                     FirstName = emp.FirstName,
+                     LastName = emp.LastName,
+                     AnnualSalary = emp.AnnualSalary,
+                     Manager = emp.IsManager,
+                     Department = emp.DepartmentId,
+                 };
+
+foreach (var employee in resultList)
 {
-
-    Console.WriteLine($"Id: {deparment.Id}");
-    Console.WriteLine($"Short Name: {deparment.ShortName}");
-    Console.WriteLine($"Long Name: {deparment.LongName}");
+    Console.WriteLine($"First Name: {employee.FirstName}");
+    Console.WriteLine($"Last Name: {employee.LastName}");
+    Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+    Console.WriteLine($"Manager: {employee.Manager}");
+    Console.WriteLine($"Department: {employee.Department}");
     Console.WriteLine();
-};
+}
+
+var averageAnnualSalary = resultList.Average(a => a.AnnualSalary);
+var highestAnnualSalary = resultList.Max(a => a.AnnualSalary);
+var lowestAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+Console.WriteLine($"Avergage annual salary: {averageAnnualSalary}");
+Console.WriteLine($"Highest annual salary: {highestAnnualSalary}");
+Console.WriteLine($"Lowest annual salary: {lowestAnnualSalary}");
